cbbd9c49d192fc00f107145d5253fdf60cc770c5
Paulo AC <zipizap123@gmail.com>
2026-09-17 12:07:05 +0200

    cleanup

--- devices/WIP/openwrts/xbox/unblock.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Demo stub: unblocks the device (real impl would e.g. remove a firewall rule).
-dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-rm -f "${dir}/.blocked"
-exit 0
